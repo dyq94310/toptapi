331e8bef9c7f91984991de5fc2645e3f0a696e1a
dyq <dyq94310@foxmail.com>
2024-03-22 17:12:35 +0800

    update

--- src/app.js
+++ src/app.js
@@ -15,12 +15,37 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+var max = 5;
+var counter = 0;
+const OTPAuth = require('otpauth');
+let totp = new OTPAuth.TOTP({
+  digits: 6,
+  period: 30,
+  secret: "SJKJKSIUIUSJHJHJH",
+});
+
+
+
 app.get('/', (req, res) => {
+  if (counter >= max) {
+    res.json({
+      message: "Max requests reached",
+    });
+  } else {
+    counter++;
+  }
+
   res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
+    message: totp.generate(),
   });
 });
 
+app.get('/reset', (req, res) => {
+  counter=0;
+  res.json({
+    message: "reset OK",
+  });
+});
 app.use('/api/v1', api);
 
 app.use(middlewares.notFound);
